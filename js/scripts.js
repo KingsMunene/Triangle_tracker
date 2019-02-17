8944b713a71fe8bc4c5c3422d1048c617d72a9d4
Kings Munene <47323582+KingsMunene@users.noreply.github.com>
2019-02-17 13:34:46 +0300

    scripts.js
    function update

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ var side1 = (document.getElementById("sidea").value);
 var side2 = (document.getElementById("sideb").value);
 var side3 = (document.getElementById("sidec").value);
 var dg = (document.getElementsByClassName("DG").value);
-if(side1==side2 && side2==side3){
+if(side1==side2 && side2==side3 && (side1+side2+side3)<=0 {
 		alert("equilateral")
   }
 else if(side1==side2 || side2 ==side3 || side1==side3){
 		alert("isosceles")
 		}
-else	if((side1 + side2) <= side3 && (side1 + side3) <= side2 || (side2 + side3) <= side1) || (side1+side2+side3)<=0{
+else	if((side1 + side2) <= side3 && (side1 + side3) <= side2 || (side2 + side3) <= side1){
     alert("Not a definite triangle")
 		}
 else{
