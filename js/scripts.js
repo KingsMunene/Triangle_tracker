ecf3ec480ca241a66078e2f43a0b76df59a17cf7
Kings Munene <47323582+KingsMunene@users.noreply.github.com>
2019-02-17 13:36:04 +0300

    scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var side1 = (document.getElementById("sidea").value);
 var side2 = (document.getElementById("sideb").value);
 var side3 = (document.getElementById("sidec").value);
 var dg = (document.getElementsByClassName("DG").value);
-if(side1==side2 && side2==side3 && (side1+side2+side3)<=0 {
+if(side1==side2 && side2==side3 && (side1+side2+side3)>0 {
 		alert("equilateral")
   }
 else if(side1==side2 || side2 ==side3 || side1==side3){
